8b741dfd46d7771acf693973528dcd2f16594d88
Vinayak Babu Rao <vinayakbaburao@gmail.com>
2021-10-03 16:37:10 +0300

    Update SpeedRacer.cs
    Added the functions

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -4,9 +4,36 @@ using UnityEngine;
 
 public class SpeedRacer : MonoBehaviour
 {
+    string carModel = "GTR R35";
+    string engineType = "V6, Twin Turbo";
+    int carWeigh = 1609;
+    int yearMade = 2009;
+    decimal maxAcceleration = 0.98;
+    bool isCarTypeSedan = false;
+    bool hasFrontEngine = true;
+
+    // Created functions
+    int CheckWeight;
+    int CalculateAge;
+
+    CheakWeight
+        {
+         if{
+            carWeight<1500;
+            print("The"carModel" weighs less than 1500kg.");
+            }
+          else
+            {
+              print("The"carModel" weighs over than 1500kg.");
+            }
     // Start is called before the first frame update
     void Start()
     {
+        print("The Car model is "carModel"and the engine Type is"engineType);
+        print(CheckWeight);
+        
+        
+        
         
     }
 
