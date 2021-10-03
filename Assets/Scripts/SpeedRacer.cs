402be883356d1fc6a47f79d67a6c384fd76e8c58
Vinayak Babu Rao <vinayakbaburao@gmail.com>
2021-10-03 23:13:40 +0300

    Update SpeedRacer.cs
    Made all the necessary functions for the assignment.

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -6,7 +6,7 @@ public class SpeedRacer : MonoBehaviour
 {
     string carModel = "GTR R35";
     string engineType = "V6, Twin Turbo";
-    int carWeigh = 1609;
+    int carWeight = 1609;
     int yearMade = 2009;
     decimal maxAcceleration = 0.98;
     bool isCarTypeSedan = false;
@@ -15,25 +15,57 @@ public class SpeedRacer : MonoBehaviour
     // Created functions
     int CheckWeight;
     int CalculateAge;
+    int carAge;
+    string CheckCharacteristics;
 
-    CheakWeight
+    CheakWeight()
         {
          if{
-            carWeight<1500;
+            carWeight < 1500;
             print("The"carModel" weighs less than 1500kg.");
             }
           else
             {
               print("The"carModel" weighs over than 1500kg.");
             }
+        }
+    
+    CalculateAge()
+    {
+      carAge=2021-yearMade;
+    }
+
+    CheckCharacteristics()
+    {
+      if{
+          isCarTypeSedan=true;
+          print("The Car type is a sedan");
+        }
+      else if{
+              hasFrontEngine = true;
+              print("The Car type is not a sedan but has a front engine");         
+             }
+      else{
+            print("The car is neither a sedan nor does it have a front engine.");
+          }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         print("The Car model is "carModel"and the engine Type is"engineType);
         print(CheckWeight);
-        
-        
-        
+        if
+          {
+            yearMade<=2009;
+            print("The Car was introduced in "yearMade);
+          }
+        CalculateAge()
+        {
+        print("The age of the car is "carAge);
+        }
+        print("The maximum acceleration of the car is"maxAcceleration);  
+        print(CheckCharacteristics);
         
     }
 
